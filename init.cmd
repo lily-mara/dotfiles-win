94b3dd336ff918ddbf94420376c9a1e2fea3a831
Nate Mara <natemara@gmail.com>
2014-01-31 12:45:34 -0500

    added vimfiles as git submodule & updated script

--- init.cmd
+++ init.cmd
@@ -3,5 +3,9 @@ echo Setting up global gitignore file
 git config --global core.excludesfile %SYSTEMDRIVE%%HOMEPATH%\.dotfiles\.global_gitignore
 
 echo.
+echo Linking vim config in user home directory
+mklink /d %SYSTEMDRIVE%%HOMEPATH%\vimfiles %SYSTEMDRIVE%%HOMEPATH%\.dotfiles\vimfiles\
+mklink /d %SYSTEMDRIVE%%HOMEPATH%\.vim %SYSTEMDRIVE%%HOMEPATH%\.dotfiles\vimfiles\
+echo.
 echo Press ENTER to exit
 pause > nul
