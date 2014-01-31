257b746d5ab230e2b09a80b2b9bc19d88a39b2c8
Nate Mara <natemara@gmail.com>
2014-01-31 13:16:01 -0500

    updated init script

--- init.cmd
+++ init.cmd
@@ -6,6 +6,7 @@ echo.
 echo Linking vim config in user home directory
 mklink /d %SYSTEMDRIVE%%HOMEPATH%\vimfiles %SYSTEMDRIVE%%HOMEPATH%\.dotfiles\vimfiles\
 mklink /d %SYSTEMDRIVE%%HOMEPATH%\.vim %SYSTEMDRIVE%%HOMEPATH%\.dotfiles\vimfiles\
+mklink %SYSTEMDRIVE%%HOMEPATH%\.vrapperrc %STEMDRIVE%%HOMEPATH%\.dotfiles\vimfiles\.vrapperrc
 echo.
 echo Press ENTER to exit
 pause > nul
