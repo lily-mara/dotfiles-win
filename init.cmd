3ca3988c6134be9ebbb14b78878866e8508e9a1e
Nate Mara <natemara@gmail.com>
2014-03-24 20:44:35 -0400

    updated init script

--- init.cmd
+++ init.cmd
@@ -4,9 +4,8 @@ git config --global core.excludesfile %SYSTEMDRIVE%%HOMEPATH%\.dotfiles\.global_
 
 echo.
 echo Linking vim config in user home directory
-mklink /d %SYSTEMDRIVE%%HOMEPATH%\vimfiles %SYSTEMDRIVE%%HOMEPATH%\.dotfiles\vimfiles\
-mklink /d %SYSTEMDRIVE%%HOMEPATH%\.vim %SYSTEMDRIVE%%HOMEPATH%\.dotfiles\vimfiles\
-mklink %SYSTEMDRIVE%%HOMEPATH%\.vrapperrc %STEMDRIVE%%HOMEPATH%\.dotfiles\vimfiles\.vrapperrc
+mklink /d %SYSTEMDRIVE%%HOMEPATH%\.vim %SYSTEMDRIVE%%HOMEPATH%\vimfiles\
+mklink %SYSTEMDRIVE%%HOMEPATH%\.vrapperrc %STEMDRIVE%%HOMEPATH%\vimfiles\.vrapperrc
 echo.
 echo Press ENTER to exit
 pause > nul
